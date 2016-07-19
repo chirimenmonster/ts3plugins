238218fa1e0359cea0941cf47f975124a5779316
chirimenmonster <chirimenmonster@users.noreply.github.com>
2016-07-19 20:36:48 +0900

    置換関数の呼び出し方法を修正

--- nyushitsu/src/filters.cpp
+++ nyushitsu/src/filters.cpp
@@ -8,34 +8,36 @@
 #include "filters.h"
 
 
+static int regex_replace(wchar_t *src, const wchar_t *regex, const wchar_t *fmt, wchar_t *dst, size_t size)
+{
+	std::wregex re(regex);
+	std::wstring result = std::regex_replace(src, re, fmt);
+	wcscpy_s(dst, size, result.c_str());
+	return 0;
+}
+
 int filter_strip(const char *string, char *result, size_t size)
 {
 	int len;
 	wchar_t wcsbuf[MAX_NICKNAME];
 
-	len = MultiByteToWideChar(CP_UTF8, 0, string, -1, wcsbuf, sizeof(wcsbuf)/sizeof(wcsbuf[0]));
+	len = MultiByteToWideChar(CP_UTF8, 0, string, -1, wcsbuf, MAX_NICKNAME);
 	if (len == 0) {
 		logMessage(u8"ワイド文字に変換できませんでした: '%s'", string);
 		return -1;
 	}
 
 	// 0: ハイフンとアンダースコアを空白に変換
-	std::wregex re0(L"[-_－＿]");
-	wchar_t fmt0[] = L" ";
-	std::wstring wresult0 = std::regex_replace(wcsbuf, re0, fmt0);
+	regex_replace(wcsbuf, L"[-_－＿]", L" ", wcsbuf, MAX_NICKNAME);
 
 	// 1: 先頭の記号と名前文字列の後に続く記号以下を削除
-	std::wregex re1(L"[^[:alpha:]]*([[:alnum:][:blank:]]*).*");
-	wchar_t fmt1[] = L"$1";
-	std::wstring wresult1 = std::regex_replace(wresult0, re1, fmt1);
+	regex_replace(wcsbuf, L"[^[:alpha:]]*([[:alnum:][:blank:]]*).*", L"$1", wcsbuf, MAX_NICKNAME);
 
 	// 2: 数字を空白に変換
-	std::wregex re2(L"[[:digit:]][[:digit:]]*");
-	wchar_t fmt2[] = L" ";
-	std::wstring wresult2 = std::regex_replace(wresult1, re2, fmt2);
+	regex_replace(wcsbuf, L"[[:digit:]][[:digit:]]*", L" ", wcsbuf, MAX_NICKNAME);
 
 
-	len = WideCharToMultiByte(CP_UTF8, 0, wresult2.c_str(), -1, result, size, NULL, NULL);
+	len = WideCharToMultiByte(CP_UTF8, 0, wcsbuf, -1, result, size, NULL, NULL);
 	if (len == 0) {
 		logMessage(u8"フィルタ後の文字列をUTF-8に変換できませんでした: 元の文字列 '%s'", string);
 		return -1;
