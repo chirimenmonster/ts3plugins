f588bdc581d895ed0ec81a15d380db8bf03f8ec5
chirimenmonster <chirimenmonster@users.noreply.github.com>
2019-06-19 21:44:54 +0900

    import API 22, fake 23

--- nyushitsu/src/plugin.c
+++ nyushitsu/src/plugin.c
@@ -32,7 +32,7 @@ struct TS3Functions ts3Functions;
 #define _strcpy(dest, destSize, src) { strncpy(dest, src, destSize-1); (dest)[destSize-1] = '\0'; }
 #endif
 
-#define PLUGIN_API_VERSION 21
+#define PLUGIN_API_VERSION 23
 
 #define PATH_BUFSIZE 512
 #define COMMAND_BUFSIZE 128
